c98bfe6f4523a4cf54feb76758979be5eac5b7e6
Lourdes Juarez <ljp@pepo.local>
2024-01-22 21:19:32 +0100

    Change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://lourdesjupo.github.io/',
-  base:'/PF_Astro/'
+  base:'/PF_Astro',
+  outDir: 'docs'
 });
