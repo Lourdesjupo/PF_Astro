40b65a9c5efd1d51844e895a971be13b36940d90
Lourdes Juarez <ljp@pepo.local>
2024-01-22 20:53:02 +0100

    Set the site and base options in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site:'https://lourdesjupo.github.io/',
+  base:'/PF_Astro/'
 });
